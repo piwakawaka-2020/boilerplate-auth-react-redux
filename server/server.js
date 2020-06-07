a7653c6c6ad77fbe5803694bb34edcce6abeb812
Boyd_Thurston <boydthurston@gmail.com>
2020-06-08 11:19:23 +1200

    add webpack config

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
+// import external modules
 const path = require('path')
 const express = require('express')
 
 const server = express()
 
+// add middleware
 server.use(express.json())
 server.use(express.static(path.join(__dirname, '../public')))
 
