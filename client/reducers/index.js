37916f3157655dd2cbd7e5683f418c8c662676b9
Boyd_Thurston <boydthurston@gmail.com>
2020-06-08 21:23:08 +1200

    add combined reducer

---
+++ client/reducers/index.js
@@ -0,0 +1,12 @@
+// import external modules
+import { combineReducers } from 'redux'
+
+// local imports
+import user from './user'
+
+// combine reducers to single reducer
+const reducer = combineReducers({
+  user
+})
+
+// export single reducer
