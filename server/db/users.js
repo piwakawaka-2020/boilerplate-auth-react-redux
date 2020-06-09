604105f572e14429631702ae2c2df750fbfb30d2
Boyd_Thurston <boydthurston@gmail.com>
2020-06-09 19:17:45 +1200

    add generate hash functionaity to db function

--- server/db/users.js
+++ server/db/users.js
@@ -1,9 +1,11 @@
 // local imports
 const connection = require('./connection')
+const {generateHash} = require('../auth/hash')
 
 // users table db functions
 function createUser ({username, password}, db = connection) {
-  return db('users').insert({username, hash: password})
+  return generateHash(password)
+    .then(hash => db('users').insert({username, hash}))
 }
 
 // export functions
