fd513fee84a75f1ea8ca0f2d767d6323a0f90096
Boyd_Thurston <boydthurston@gmail.com>
2020-06-09 15:06:17 +1200

    add auth routes

---
+++ server/routes/auth.js
@@ -0,0 +1,15 @@
+// import external modules
+const express = require('express')
+
+// define router
+const router = express.Router()
+
+// define routes
+router.post('/register', register)
+
+// supporting functions to routs
+function register (req,res) {
+  const { username, password } = req.body
+}
+
+// export router
