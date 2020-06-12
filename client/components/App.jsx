4f3d76527e0440f37f1e042dc6e9e094f3b6aae5
Boyd_Thurston <boydthurston@gmail.com>
2020-06-12 15:56:05 +1200

    add utils lib token functions

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,24 @@
 // import external modules
 import React from 'react'
 import { connect } from 'react-redux'
+import { getToken, isAuthenticated } from '../utils/lib'
 
 // local imports
 
 // define class component 
 export class App extends React.Component {
   render(){
+    const token = getToken()
     return(
       <>
         <h1>Development has begun</h1>
         <h2>Behold I am the App component</h2>
+        <p>{token}</p>
+        {isAuthenticated() && <button>Log off</button>}
+        {!isAuthenticated() && <>
+          <button>Sign in</button>
+          <button>Register</button>
+        </>}
       </>
     )
   }
---
+++ client/utils/lib.js
@@ -0,0 +1,18 @@
+
+// captures JWT details from local store
+export function getToken () {
+  const localStorage = global.window.localStorage
+  const localStorageKeyName = 'token'
+  return localStorage.getItem(localStorageKeyName)
+}
+
+// checks to see if user is authenticated or not
+export function isAuthenticated () {
+  const authToken = getToken()
+  console.log(authToken)
+  if(authToken != null){
+    return true
+  } else {
+    return false
+  }
+}
