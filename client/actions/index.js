0c91fa3d1c80fca91c4246279398a25dde5b63f8
Boyd_Thurston <boydthurston@gmail.com>
2020-06-16 14:21:17 +1200

    add sign in functionality to actions

--- client/actions/index.js
+++ client/actions/index.js
@@ -4,4 +4,21 @@ import { addNewUser, userSignIn, getUserDetails } from '../apis'
 // export action calls
 export const SAVE_USER = 'SAVE_USER'
 
-// define action functions
+// define dispatch actions
+export function saveUser(details){
+  return {
+    type: SAVE_USER,
+    username: details.username,
+  }
+}
+
+// define Thunk functions
+
+// sign in function
+export function logIn(creds){
+  return dispatch => {
+    userSignIn(creds)
+    .then(() => getUserDetails())
+    .then(userDetails => {dispatch(saveUser(userDetails))})
+  }
+}
--- client/components/SignIn.jsx
+++ client/components/SignIn.jsx
@@ -2,27 +2,39 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+// local imports
+import { logIn } from '../actions'
+
+// define class component
 class SignIn extends React.Component {
+  // create initial local state
   state = {
     username: '',
     pasword: ''
   }
 
+  // handler for input events
   handleChange = event => {
     this.setState({[event.target.name]: event.target.value})
   }
 
+  // hander for submit event
   handleSubmit = event => {
     event.preventDefult()
+    this.props.dispatch(logIn)
   }
 
+  // display component
   render(){
     return <form onSubmit={this.handleSubmit}>
       <label>username:</label>
       <input type='text' onChange={this.handleChange} name='username' value={this.state.username}/>
       <label>username:</label>
-      <input type='text' onChange={this.handleChange} name='username' value={this.state.username}/>
+      <input type='password' onChange={this.handleChange} name='username' value={this.state.username}/>
       <input type='submit' value='Sign in'/>
     </form>
   }
 }
+
+// export component
+export default connect()(SignIn)
