61d1786fd3e2784496c561e546f368704f68f697
Boyd_Thurston <boydthurston@gmail.com>
2020-06-16 12:48:19 +1200

    add API

--- client/apis/index.js
+++ client/apis/index.js
@@ -1,1 +1,19 @@
+// import external modules
 import request from 'superagent'
+
+// define routes URLS
+const baseUrl = '/api/v1/'
+const authRoute = '/auth/'
+
+// api calls
+
+// reister a new user
+export function addNewUser(user) {
+  return request.post(`${baseUrl}${authRoute}register`)
+    .send(user)
+    .then(res => res.body)
+}
+
+// verify a users credentails
+
+// get a varified users details
