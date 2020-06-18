70c5e3429e3a395c70894ce1096668b521de9717
Boyd_Thurston <boydthurston@gmail.com>
2020-06-19 11:48:20 +1200

    refactor Utils

--- client/utils/lib.js
+++ client/utils/lib.js
@@ -1,3 +1,12 @@
+// UTIITIES LIBARY FOR FRONTEND SUPPORTING FUNCTIONS
+
+
+// _____ AUTH/JWT FUNCTIONS ______
+
+// set JWT token in local storage
+export function setToken (token) {
+  localStorage.setItem('token', token)
+}
 
 // captures JWT details from local store
 export function getToken () {
@@ -7,15 +16,9 @@ export function getToken () {
 // checks to see if user is authenticated or not
 export function isAuthenticated () {
   const authToken = getToken()
-  console.log(authToken)
   if(authToken != null){
     return true
   } else {
     return false
   }
-}
-
-// set JWT token in local storage
-export function setToken (token) {
-  localStorage.setItem('token', token)
 }
